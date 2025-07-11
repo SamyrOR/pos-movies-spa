9446bd0ee6a085bde611d4bad03efe3395041f27
Samyr Ribeiro <saamyr@live.com>
2025-07-10 21:24:31 -0300

    feat: updated movies page with most popular movies

--- src/views/Movies.tsx
+++ src/views/Movies.tsx
@@ -1,3 +1,22 @@
+import { useEffect, useState } from "react";
+import { getPopularMovies } from "../services/movies.service";
+import type { Movie } from "../model/movie.model";
+
 export function Movies() {
-  return <h1>Movies</h1>
+  const [movies, setMovies] = useState<Movie[]>([]);
+
+  useEffect(() => {
+    getPopularMovies().then(({ data }) => {
+      setMovies(data.results)
+    })
+  }, [])
+
+  return (
+    <section>
+      <h1>Filmes populares</h1>
+      <ul>
+        {movies.map((movie) => <li>{movie.title}</li>)}
+      </ul>
+    </section>
+  );
 }
