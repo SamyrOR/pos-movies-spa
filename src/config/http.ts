102399090dce83403b50f918a75063e936c79eb5
Samyr Ribeiro <saamyr@live.com>
2025-07-10 22:27:52 -0300

    refactor: removed hard coded token, and moved it to .env variable

--- src/config/http.ts
+++ src/config/http.ts
@@ -3,6 +3,6 @@ import axios from "axios";
 export const moviesApi = axios.create({
   baseURL: 'https://api.themoviedb.org/3/',
   headers: {
-    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyYmZmMWMxOWNmZmEzYTg4N2Y4ZDg1ZDEwMjVjODA1YSIsIm5iZiI6MTc1MTkzNzY2MC4wMDEsInN1YiI6IjY4NmM3MjdiMmZkMTIzMjY4MjcwMzIyOCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Sm0mqFaiyj0q_h4wpt0Ya1TyQxQkJQoc7wvWSJzGKNo'
+    Authorization: `Bearer ${import.meta.env.VITE_TMDB_ACCESS_TOKEN}`
   }
 })
